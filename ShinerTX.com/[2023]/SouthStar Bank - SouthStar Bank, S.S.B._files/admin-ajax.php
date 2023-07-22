f90afe7f0075f6d9021e6e2aa061107648e965db
UmberonCircle <140108041+UmberonWithCircles@users.noreply.github.com>
2023-07-21 22:45:45 -0500

    ShinerTX.com (12)

---
+++ ShinerTX.com/[2023]/SouthStar Bank - SouthStar Bank, S.S.B._files/admin-ajax.php
@@ -0,0 +1,643 @@
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-grid-skin .efbl-row.e-outer {
+    grid-template-columns: repeat(auto-fill, minmax(33.33%, 1fr));
+}
+
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_feeds_holder.efbl_feeds_carousel {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_feeds_holder.efbl_feeds_carousel .owl-nav {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots {
+
+ display: block;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots .owl-dot span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots .owl-dot.active span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_load_more_holder a.efbl_load_more_btn span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_load_more_holder a.efbl_load_more_btn:hover span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_header {
+
+ box-shadow: none;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_header_title {
+
+
+}
+
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_header .efbl_header_inner_wrap .efbl_header_img img {
+
+ border-radius: 0;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_cat, .efbl_feed_wraper.efbl_skin_3785 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_followers {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_bio {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-thumbnail-wrapper .efbl-thumbnail-col, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer {
+
+
+}
+
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper {
+    box-shadow: none;
+}
+
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-thumbnail-wrapper .efbl-thumbnail-col a img {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3785 .efbl_feeds_carousel .efbl-story-wrapper .efbl-grid-wrapper {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-feed-content > .efbl-d-flex .efbl-profile-title span, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-feed-content .description, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-feed-content .description a, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-feed-content .efbl_link_text, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-feed-content .efbl_link_text .efbl_title_link a {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer .efbl-reacted-item, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer .efbl-reacted-item .efbl_all_comments_wrap {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-overlay {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-overlay .efbl_multimedia, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-overlay .icon-esf-video-camera {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer .efbl-view-on-fb, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer .esf-share-wrapper .esf-share {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer .efbl-view-on-fb:hover, .efbl_feed_wraper.efbl_skin_3785 .efbl-story-wrapper .efbl-post-footer .esf-share-wrapper .esf-share:hover {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-caption::after {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-caption .efbl-feed-description, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 a, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 span {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-post-header {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-post-header .efbl-profile-image {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-post-header h2 {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-post-header span {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-feed-description, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl_link_text {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-reactions-box {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions span {
+
+
+}
+
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions .efbl_popup_likes_main {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions .efbl-popup-comments-icon-wrapper {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-commnets, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-comments-list {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-action-btn {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3785 .efbl-d-columns-wrapper .efbl-comments-list .efbl-comment-wrap {
+
+
+}
+
+
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-grid-skin .efbl-row.e-outer {
+    grid-template-columns: repeat(auto-fill, minmax(33.33%, 1fr));
+}
+
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_feeds_holder.efbl_feeds_carousel {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_feeds_holder.efbl_feeds_carousel .owl-nav {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots {
+
+ display: block;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots .owl-dot span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots .owl-dot.active span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_load_more_holder a.efbl_load_more_btn span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_load_more_holder a.efbl_load_more_btn:hover span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_header {
+
+ box-shadow: none;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_header_title {
+
+
+}
+
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_header .efbl_header_inner_wrap .efbl_header_img img {
+
+ border-radius: 0;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_cat, .efbl_feed_wraper.efbl_skin_3786 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_followers {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_bio {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-thumbnail-wrapper .efbl-thumbnail-col, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer {
+
+
+}
+
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper {
+    box-shadow: none;
+}
+
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-thumbnail-wrapper .efbl-thumbnail-col a img {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3786 .efbl_feeds_carousel .efbl-story-wrapper .efbl-grid-wrapper {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-feed-content > .efbl-d-flex .efbl-profile-title span, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-feed-content .description, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-feed-content .description a, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-feed-content .efbl_link_text, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-feed-content .efbl_link_text .efbl_title_link a {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer .efbl-reacted-item, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer .efbl-reacted-item .efbl_all_comments_wrap {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-overlay {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-overlay .efbl_multimedia, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-overlay .icon-esf-video-camera {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer .efbl-view-on-fb, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer .esf-share-wrapper .esf-share {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer .efbl-view-on-fb:hover, .efbl_feed_wraper.efbl_skin_3786 .efbl-story-wrapper .efbl-post-footer .esf-share-wrapper .esf-share:hover {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-caption::after {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-caption .efbl-feed-description, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 a, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 span {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-post-header {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-post-header .efbl-profile-image {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-post-header h2 {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-post-header span {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-feed-description, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl_link_text {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-reactions-box {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions span {
+
+
+}
+
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions .efbl_popup_likes_main {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions .efbl-popup-comments-icon-wrapper {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-commnets, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-comments-list {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-action-btn {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3786 .efbl-d-columns-wrapper .efbl-comments-list .efbl-comment-wrap {
+
+
+}
+
+
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-grid-skin .efbl-row.e-outer {
+    grid-template-columns: repeat(auto-fill, minmax(33.33%, 1fr));
+}
+
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_feeds_holder.efbl_feeds_carousel {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_feeds_holder.efbl_feeds_carousel .owl-nav {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots {
+
+ display: block;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots .owl-dot span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_feeds_holder.efbl_feeds_carousel .owl-dots .owl-dot.active span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_load_more_holder a.efbl_load_more_btn span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_load_more_holder a.efbl_load_more_btn:hover span {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_header {
+
+ box-shadow: none;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_header_title {
+
+
+}
+
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_header .efbl_header_inner_wrap .efbl_header_img img {
+
+ border-radius: 0;
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_cat, .efbl_feed_wraper.efbl_skin_3787 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_header_meta .efbl_followers {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl_header .efbl_header_inner_wrap .efbl_header_content .efbl_bio {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-thumbnail-wrapper .efbl-thumbnail-col, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer {
+
+
+}
+
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper {
+    box-shadow: none;
+}
+
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-thumbnail-wrapper .efbl-thumbnail-col a img {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3787 .efbl_feeds_carousel .efbl-story-wrapper .efbl-grid-wrapper {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-feed-content > .efbl-d-flex .efbl-profile-title span, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-feed-content .description, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-feed-content .description a, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-feed-content .efbl_link_text, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-feed-content .efbl_link_text .efbl_title_link a {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer .efbl-reacted-item, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer .efbl-reacted-item .efbl_all_comments_wrap {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-overlay {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-overlay .efbl_multimedia, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-overlay .icon-esf-video-camera {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer .efbl-view-on-fb, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer .esf-share-wrapper .esf-share {
+
+
+}
+
+.efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer .efbl-view-on-fb:hover, .efbl_feed_wraper.efbl_skin_3787 .efbl-story-wrapper .efbl-post-footer .esf-share-wrapper .esf-share:hover {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-caption::after {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-caption .efbl-feed-description, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 a, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 span {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-post-header {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-post-header .efbl-profile-image {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-post-header h2 {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-post-header span {
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-feed-description, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl_link_text {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-reactions-box {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions span {
+
+
+}
+
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions .efbl_popup_likes_main {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-reactions-box .efbl-reactions .efbl-popup-comments-icon-wrapper {
+
+ display: flex;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-commnets, .efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-comments-list {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-action-btn {
+
+ display: block;
+
+
+}
+
+.efbl_feed_popup_container .efbl-post-detail.efbl-popup-skin-3787 .efbl-d-columns-wrapper .efbl-comments-list .efbl-comment-wrap {
+
+
+}
+
+
+  
