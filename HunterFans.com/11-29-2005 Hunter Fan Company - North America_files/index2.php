9c7e868a83187556b76e84b19cfe736b6e8af447
UmberonCircle <140108041+UmberonWithCircles@users.noreply.github.com>
2023-07-21 23:17:31 -0500

    Hunterfans.com (3)

---
+++ HunterFans.com/11-29-2005 Hunter Fan Company - North America_files/index2.php
@@ -0,0 +1,285 @@
+		
+		
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+
+<html>
+
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=ISO-8859-1">
+		<title>Hunter Fan Company - North America</title>
+		<script src="http://www.hunterfan.com/js/master.js"></script>
+		<link rel="stylesheet" href="http://www.hunterfan.com/css/master.css">
+	</head>
+
+	<body bgcolor="#ffffff" leftmargin="0" marginwidth="0" topmargin="0" marginheight="0" class="background" onload="wName('HunterMain');">		<style media="screen" type="text/css"><!--
+		.blacktable { background-color: #000; padding: 1px }
+		.whitetable { background-color: #fff; padding: 2px }
+		body   { background: #fff1bd url(http://www.hunterfan.com/media/home/home_back.jpg) no-repeat scroll }
+		.background  { background: #fff1bd url(http://www.hunterfan.com/media/home/home_back.jpg) no-repeat scroll }
+		.bottomtext { color: #556b61; font-size: 10px; font-family: Verdana, Arial, Helvetica; text-decoration: none; padding: 5px }
+		--></style>
+		<table border="0" cellpadding="0" cellspacing="0" width="100%" height="100%">
+			<tr>
+				<td valign="top" align="left">
+					<table border="0" cellpadding="0" cellspacing="0" width="800" height="150">
+						<tr>
+							<td valign="top">
+								<table border="0" cellpadding="0" cellspacing="0" width="800">
+									<tr>
+										<td width="350"></td>
+										<td valign="top" align="right">
+											<br>
+											<br>
+											<br>
+											<form name="navCnt">
+											<select name="countryNav" size="1" onchange="navCountry();"><option value="">Choose a Country</option>
+<option value="--Americas--">--Americas--</option>
+<option value="USA">United States</option>
+<option value="Can">Canada</option>
+<option value="6">Colombia</option>
+<option value="7">Costa Rica</option>
+<option value="8">Cura&ccedil;ao</option>
+<option value="10">Ecuador</option>
+<option value="46">Guatemala</option>
+<option value="11">Honduras</option>
+<option value="13">Mexico</option>
+<option value="14">Panama</option>
+<option value="62">VENEZUELA</option>
+<option value=" "> </option>
+<option value="--Asia Pacific--">--Asia Pacific--</option>
+<option value="20">Australia</option>
+<option value="21">Christmas Island (Aus)</option>
+<option value="22">Cocos (Keeling) Islands (Aus)</option>
+<option value="23">Fiji</option>
+<option value="24">India</option>
+<option value="49">Indonesia</option>
+<option value="25">Japan</option>
+<option value="26">New Zealand</option>
+<option value="27">Norfolk Island (Aus)</option>
+<option value="28">Pakistan</option>
+<option value="29">Philippines</option>
+<option value=" "> </option>
+<option value="--Western Europe--">--Western Europe--</option>
+<option value="30">Austria</option>
+<option value="31">Belgium</option>
+<option value="59">Denmark</option>
+<option value="58">Finland</option>
+<option value="32">France</option>
+<option value="33">Germany</option>
+<option value="48">Iceland</option>
+<option value="34">Ireland</option>
+<option value="63">Italy</option>
+<option value="57">Norway</option>
+<option value="35">Scotland</option>
+<option value="36">Spain</option>
+<option value="56">Sweden</option>
+<option value="37">Switzerland</option>
+<option value="38">United Kingdom</option>
+<option value=" "> </option>
+<option value="--Mid-Atlantic/Caribbean--">--Mid-Atlantic/Caribbean--</option>
+<option value="44">Bahamas</option>
+<option value="2">Barbados</option>
+<option value="3">Bermuda</option>
+<option value="5">Cayman Islands</option>
+<option value="9">Dominican Republic</option>
+<option value="45">Grenada</option>
+<option value="47">Haiti</option>
+<option value="12">Jamaica</option>
+<option value="50">Puerto Rico</option>
+<option value="15">Saint Barts</option>
+<option value="51">Saint Croix</option>
+<option value="16">Saint Johns</option>
+<option value="17">Saint Maarten (Dutch Side)</option>
+<option value="52">Saint Maarten (French Side)</option>
+<option value="19">Trinidad and Tobago</option>
+<option value="53">Turk and Caicos Islands</option>
+<option value=" "> </option>
+<option value="--Central & Eastern Europe--">--Central & Eastern Europe--</option>
+<option value="39">Cyprus</option>
+<option value="40">Greece</option>
+<option value="54">Ukraine</option>
+<option value=" "> </option>
+<option value="--Middle East & Africa--">--Middle East & Africa--</option>
+<option value="41">Israel</option>
+<option value="61">Mauritius</option>
+<option value="42">Morocco</option>
+<option value="43">United Arab Emirates</option>
+<option value=" "> </option>
+</select><br>											</form>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+					</table>
+					<table border="0" cellpadding="0" cellspacing="0" height="207">
+						<tr height="18">
+							<td width="5" height="18"></td>
+							<td nowrap height="18">
+								<div align="right">
+									<table border="0" cellpadding="0" cellspacing="0" width="341">
+										<tr>
+											<td align="right">
+												<div align="right">
+													<a href="about.php" onmouseover="rOver('about','http://www.hunterfan.com/media/home/tnav/about2.gif');" onmouseout="rOver('about','http://www.hunterfan.com/media/home/tnav/about.gif');"><img name="about" src="http://www.hunterfan.com/media/home/tnav/about.gif" width="73" height="18" border="0"></a><a href="products.php" onmouseover="rOver('products','http://www.hunterfan.com/media/home/tnav/products2.gif');" onmouseout="rOver('products','http://www.hunterfan.com/media/home/tnav/products.gif');"><img name="products" src="http://www.hunterfan.com/media/home/tnav/products.gif" width="70" height="19" border="0"></a><a href="support.php" onmouseover="rOver('support','http://www.hunterfan.com/media/home/tnav/support2.gif');" onmouseout="rOver('support','http://www.hunterfan.com/media/home/tnav/support.gif');"><img name="support" src="http://www.hunterfan.com/media/home/tnav/support.gif" width="104" height="19" border="0"></a><a href="dealer.php" onmouseover="rOver('dealer','http://www.hunterfan.com/media/home/tnav/dealer2.gif');" onmouseout="rOver('dealer','http://www.hunterfan.com/media/home/tnav/dealer.gif');"><img name="dealer" src="http://www.hunterfan.com/media/home/tnav/dealer.gif" width="94" height="19" border="0"></a></div>
+											</td>
+										</tr>
+									</table>
+								</div>
+							</td>
+						</tr>
+						<tr height="163">
+
+	<td width="" height="163">&nbsp;</td>
+	<td class="blacktable" height="163" valign="middle">
+		<table border="0" cellpadding="0" cellspacing="0">
+			<tr>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="77" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/fan_button.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/fan_button.swf" type="application/x-shockwave-flash" width="77" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="77" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/purifier_button.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/purifier_button.swf" type="application/x-shockwave-flash" width="77" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="77" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/humidifier_button.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/humidifier_button.swf" type="application/x-shockwave-flash" width="77" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="77" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/thermostat_button.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/thermostat_button.swf" type="application/x-shockwave-flash" width="77" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="77" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/vaporizer_button.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/vaporizer_button.swf" type="application/x-shockwave-flash" width="77" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="77" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/portable_button.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/portable_button.swf" type="application/x-shockwave-flash" width="77" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+			<td class="whitetable">
+				<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=4,0,2,0" width="299" height="163">
+					<param name="movie" value="http://www.hunterfan.com/media/home/flashnav/flashbox.swf">
+					<param name="quality" value="best">
+					<param name="play" value="true">
+					<embed src="http://www.hunterfan.com/media/home/flashnav/flashbox.swf" type="application/x-shockwave-flash" width="299" height="163" pluginspage="http://www.macromedia.com/shockwave/download/index.cgi?P1_Prod_Version=ShockwaveFlash" quality="best" play="true">
+				</object>
+			</td>
+		</tr>
+			</table>
+				</td>
+			</tr>
+			<tr height="18">
+				<td width="5" height="18"></td>
+				<td nowrap valign="top" align="left" height="18">
+					<div align="left">
+					<a href="http://www.hunterfan.com/fans.php"><img src="http://www.hunterfan.com/media/home/fans.gif" width="77" height="18" border="0"></a><a href="http://www.hunterfan.com/purifiers.php"><img src="http://www.hunterfan.com/media/home/purifiers.gif" width="83" height="18" border="0"></a><a href="http://www.hunterfan.com/humidifiers.php"><img src="http://www.hunterfan.com/media/home/humidifiers.gif" width="79" height="18" border="0" hspace="1"></a><a href="http://www.hunterfan.com/thermostats.php"><img src="http://www.hunterfan.com/media/home/thermostats.gif" width="83" height="18" border="0" hspace="3"></a><a href="http://www.hunterfan.com/vaporizers.php"><img src="http://www.hunterfan.com/media/home/vaporizers.gif" width="77" height="18" border="0"></a><a href="http://www.hunterfan.com/portablefans.php"><img src="http://www.hunterfan.com/media/home/portable.gif" width="82" height="18" border="0"></a>
+								</div>
+							</td>
+						</tr>
+					</table>
+					<br>
+					<table border="0" cellpadding="0" cellspacing="0" width="750">
+						<tr>
+							<td width="488"></td>
+							<td width="262">
+							
+
+<!--
+<script language="JavaScript"> 
+<!-- Hide from old browsers 
+
+var imagenumber = 5 ; 
+var randomnumber = Math.random() ; 
+var rand1 = Math.round( (imagenumber-1) * randomnumber) + 1 ; 
+images = new Array 
+images[1] = "http://www.hunterfan.com/media/home/teasers/airmax2.jpg" 
+images[2] = "http://www.hunterfan.com/media/home/teasers/ezblade.jpg" 
+images[3] = "http://www.hunterfan.com/media/home/teasers/esav.jpg" 
+images[4] = "http://www.hunterfan.com/media/home/teasers/wobble.jpg" 
+images[5] = "http://www.hunterfan.com/media/home/teasers/ezglass.jpg" 
+var image = images[rand1] 
+
+links = new Array 
+links[1] = "http://www.hunterfan.com/features/airmax.php" 
+links[2] = "http://www.hunterfan.com/features/ez-blades.php" 
+links[3] = "http://www.hunterfan.com/features/energysav.php" 
+links[4] = "http://www.hunterfan.com/features/wobblefree.php" 
+links[5] = "http://www.hunterfan.com/features/ez-glass.php" 
+var link = links[rand1]// -- End Hiding Here --> 
+
+<!--</script>//-->
+								<!--<script language="JavaScript"> 
+<!-- Hide this script from old browsers --> 
+<!--document.write('<A HREF="' + link + '"><IMG SRC="' + image + '" border="0"></a>') 
+<!-- End Hiding Here --> 
+<!--</script>//-->
+								<a href="bathfans.php"><img src="media/home/home_bathfan_graphic.jpg" alt="" height="125" width="265" border="0"></a>
+						
+							</td>
+						</tr>
+					</table>
+				</td>
+			</tr>									
+			<tr height="58">
+				<td height="58" valign="bottom" background="#">
+					<table border="0" cellpadding="0" cellspacing="0" width="100%">
+						<tr height="33">
+							<td height="33" valign="bottom" nowrap colspan="2">
+								<table border="0" cellpadding="0" cellspacing="0" width="100%">
+									<tr height="33">
+										<td valign="bottom" width="218" height="33"><img src="http://www.hunterfan.com/media/home/bnav/wave_l.gif" width="218" height="33" border="0"></td>
+										<td valign="bottom" height="33" background="http://www.hunterfan.com/media/home/bnav/wave_tile.gif"><img src="http://www.hunterfan.com/media/home/bnav/wave_tile.gif" width="3" height="33" border="0"></td>
+										<td valign="bottom" height="33" width="579">
+											<div align="right">
+												<img src="http://www.hunterfan.com/media/home/bnav/wave_r.gif" width="579" height="33" border="0"></div>
+										</td>
+									</tr>
+								</table>
+							</td>
+						</tr>
+						<tr height="25">
+							<td colspan="2" background="http://www.hunterfan.com/media/home/bnav/fill.gif" height="25" valign="bottom">
+								<div align="center">
+									<table border="0" cellpadding="0" cellspacing="0" width="488">
+										<tr>
+											<td valign="bottom" nowrap><a href="contact.php" onmouseover="rOver('contact','http://www.hunterfan.com/media/home/bnav/contact2.gif');" onmouseout="rOver('contact','http://www.hunterfan.com/media/home/bnav/contact.gif');"><img name="contact" src="http://www.hunterfan.com/media/home/bnav/contact.gif" width="79" height="25" border="0"><a href="about/news.php" onmouseover="rOver('news','http://www.hunterfan.com/media/home/bnav/news2.gif');" onmouseout="rOver('news','http://www.hunterfan.com/media/home/bnav/news.gif');"><img name="news" src="http://www.hunterfan.com/media/home/bnav/news.gif" width="57" height="25" border="0"><a href="support/warranty.php" onmouseover="rOver('warranty','http://www.hunterfan.com/media/home/bnav/warranty2.gif');" onmouseout="rOver('warranty','http://www.hunterfan.com/media/home/bnav/warranty.gif');"><img name="warranty" src="http://www.hunterfan.com/media/home/bnav/warranty.gif" width="72" height="25" border="0"><a href="legal.php" onmouseover="rOver('legal','http://www.hunterfan.com/media/home/bnav/legal2.gif');" onmouseout="rOver('legal','http://www.hunterfan.com/media/home/bnav/legal.gif');"><img name="legal" src="http://www.hunterfan.com/media/home/bnav/legal.gif" width="99" height="25" border="0"><a href="sitemap.php" onmouseover="rOver('sitemap','http://www.hunterfan.com/media/home/bnav/sitemap2.gif');" onmouseout="rOver('sitemap','http://www.hunterfan.com/media/home/bnav/sitemap.gif');"><img name="sitemap" src="http://www.hunterfan.com/media/home/bnav/sitemap.gif" width="65" height="25" border="0"><a href="require.php" onmouseover="rOver('require','http://www.hunterfan.com/media/home/bnav/require2.gif');" onmouseout="rOver('require','http://www.hunterfan.com/media/home/bnav/require.gif');"><img name="require" src="http://www.hunterfan.com/media/home/bnav/require.gif" width="116" height="25" border="0"></td>
+										</tr>
+									</table>
+								</div>
+							</td>
+						</tr>
+						<tr height="15">
+														
+						</tr>
+					</table>
+				</td>
+			</tr>
+		</table>
+	</body>
+</html>
